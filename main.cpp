b718bc12af7d930b12f8d917308b385544bb7b9d
Noodle1991 <staspesetsky@gmail.com>
2022-12-01 15:50:18 +0300

    fixxed

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 
 int main() {
-    /// массив строк довольно жирненько)
-    /// можно использовать просто std::string
-    std::string stroka[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "a", "B", "b", "C", "c", "D", "d",
+    /*std::string stroka[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "a", "B", "b", "C", "c", "D", "d",
                             "E",
-                            "e", "F", "f", "G", "g", "H", "h", "X", "x", "Y", "y", "Z", "z"};
-    /// объявлять внутри цикла
-    int i, g, f, l, k;
-                    /// хардкод длинны 
-    for (i = 0; i < 32; i++) {
-        for (g = 0; g < 32; g++) {
-            for (f = 0; f < 32; f++) {
-                for (l = 0; l < 32; l++) {
-                    for (k = 0; k < 32; k++) {
+                            "e", "F", "f", "G", "g", "H", "h", "X", "x", "Y", "y", "Z", "z"};*/
+    std::string stroka = {"1234567890AaBbCcDdEeFfGgHhXxYyZz"};
+    /*int i, g, f, l, k;*/
+    for (int i = 0; i < 32; i++) {
+        for (int g = 0; g < 32; g++) {
+            for (int f = 0; f < 32; f++) {
+                for (int l = 0; l < 32; l++) {
+                    for (int k = 0; k < 32; k++) {
                         std::cout << stroka[i] << stroka[g] << stroka[f] << stroka[l] << stroka[k] << std::endl;
                     }
                 }
